4bcb25ae8b7f6b05042452e3a1efea307832c0a8
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-09-25 23:31:35 +0200

    Add timer reducer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,20 +1,44 @@
-import React from "react"
+import React from "react";
 
-const Timer = () => {
-  const isRunning = false
-  return (  
-  <div className='timer'>
-    <div className='timer__display'>
-      <span>25</span>
-      <span>:</span>
-      <span>00</span>
-    </div>
-    <div className='timer__control'>
-      <button>{isRunning ? 'PAUSE' : 'START'}</button>
-      <button>RESET</button>
-    </div>
-  </div>
-  )
-}
+const Timer = ({ duration = 25 }) => {
+  const initialState = { minutes: duration, seconds: 0, isRunning: false };
+  const actionTypes = { OPERATE: "opertate", RESTART: "restart" };
+  const timerReducer = (state, { type }) => {
+    switch (type) {
+      case actionTypes.OPERATE:
+        return {
+          ...state,
+          minutes: state.seconds === 0 ? state.minutes - 1 : state.minutes,
+          seconds: state.seconds === 0 ? 59 : state.seconds - 1,
+        };
+      case actionTypes.RESTART:
+        return { ...state, ...initialState };
 
-export {Timer}
+      default:
+        break;
+    }
+  };
+  const [{ minutes, seconds, isRunning }, dispatch] = React.useReducer(
+    timerReducer,
+    initialState
+  );
+  return (
+    <div className="timer">
+      <div className="timer__display">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div className="timer__control">
+        <button onClick={() => dispatch({ type: actionTypes.OPERATE })}>
+          {isRunning ? "PAUSE" : "START"}
+        </button>
+        <button onClick={() => dispatch({ type: actionTypes.RESTART })}>
+          RESET
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { Timer };
