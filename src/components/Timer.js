6a70f5cf341e6ad1e438cb572aa09a27bac5804a
Mohammed Al-Asfar <elasfarc@gmail.com>
2021-09-25 23:58:01 +0200

    Add action types
    handle starting and pausing the timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,26 +2,47 @@ import React from "react";
 
 const Timer = ({ duration = 25 }) => {
   const initialState = { minutes: duration, seconds: 0, isRunning: false };
-  const actionTypes = { OPERATE: "opertate", RESTART: "restart" };
+  const actionTypes = {
+    RUN: "decrease",
+    RESTART: "restart",
+    PAUSE: "pause",
+  };
   const timerReducer = (state, { type }) => {
     switch (type) {
-      case actionTypes.OPERATE:
+      case actionTypes.RUN:
         return {
           ...state,
           minutes: state.seconds === 0 ? state.minutes - 1 : state.minutes,
           seconds: state.seconds === 0 ? 59 : state.seconds - 1,
+          isRunning: true,
         };
       case actionTypes.RESTART:
         return { ...state, ...initialState };
 
+      case actionTypes.PAUSE:
+        return { ...state, isRunning: false };
+
       default:
-        break;
+        console.error("undefined type");
     }
   };
   const [{ minutes, seconds, isRunning }, dispatch] = React.useReducer(
     timerReducer,
     initialState
   );
+  const intervalRef = React.useRef();
+
+  const operate = () => {
+    if (isRunning) {
+      clearInterval(intervalRef.current);
+      dispatch({ type: actionTypes.PAUSE });
+    } else
+      intervalRef.current = setInterval(
+        () => dispatch({ type: actionTypes.RUN }),
+        1000
+      );
+  };
+
   return (
     <div className="timer">
       <div className="timer__display">
@@ -30,9 +51,7 @@ const Timer = ({ duration = 25 }) => {
         <span>{seconds}</span>
       </div>
       <div className="timer__control">
-        <button onClick={() => dispatch({ type: actionTypes.OPERATE })}>
-          {isRunning ? "PAUSE" : "START"}
-        </button>
+        <button onClick={operate}>{isRunning ? "PAUSE" : "START"}</button>
         <button onClick={() => dispatch({ type: actionTypes.RESTART })}>
           RESET
         </button>
